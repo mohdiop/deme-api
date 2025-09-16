43fe9f7f31a2d63e27fed42774fb6f6a1fb5dd38
Mohamed Diop <mohdiopcode@gmail.com>
2025-09-16 17:44:43 +0000

    Initializes project

---
+++ src/main/java/com/mohdiop/deme_api/DemeApiApplication.java
@@ -0,0 +1,13 @@
+package com.mohdiop.deme_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohdiop/deme_api/DemeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohdiop.deme_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
